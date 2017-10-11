f358845dd05155a7fa6f8bad56ef01414c11bb91
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:50:37 +0200

    object to class

--- src/test/scala/nl/biopet/tools/wipereads/WipeReadsTest.scala
+++ src/test/scala/nl/biopet/tools/wipereads/WipeReadsTest.scala
@@ -3,7 +3,7 @@ package nl.biopet.tools.wipereads
 import nl.biopet.test.BiopetTest
 import org.testng.annotations.Test
 
-object WipeReadsTest extends BiopetTest {
+class WipeReadsTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
